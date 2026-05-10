a416af6566cfcf532cecd298d67e95481d8edc9c
FiloJFry <filippomandolini14@gmail.com>
2026-05-10 20:17:00 +0200

    Refactor weapon and enemy initialization
    Updated weapon and enemy definitions with new attributes.

--- DefinizioneUtili.js
+++ DefinizioneUtili.js
@@ -2,7 +2,7 @@ const Quadrupletta = new Arma("Quadrupletta",24000,10,1,3000,false,"[####]",["BA
 const DoppiaPistolaDaClown = new Arma("Doppia pistola da clown",13000,6,2,300,false,"@ @",["Scherzetto!", "crrt", "f-flump"]);
 const SpargiSchegge = new Arma("Spargischegge",9960,20,4,1400,false,"|***|",["splsh!", "fr-ln", "fr-ln"]);
 const LanciafiammeTriforcuto = new Arma("Lanciafiamme triforcuto",4000,16,5,1,true,"^ ^ ^",["FFFSH", "", "blblblbl..."]);
-const DistributoreDiPalline = new Arma("Distributore di palline",196,75,45,20,true,"I<br/>==O==<br/>I",["Boing!", "tk", "CRT-CRT-CRT"]);
+const DistributoreDiPalline = new Arma("Distributore di palline",200,78,45,25,true,"I<br/>==O==<br/>I",["Boing!", "tk", "CRT-CRT-CRT"]);
 const SparaOssa = new Arma("Sparaossa",90,84,80,75,true,"I<br/>--X--<br/>I",["tln", "tsh!", "frrlll!"]);
 const FriggitriceDAssalto = new Arma("Friggitrice da assalto",324,64,30,56,true,"I<br/>~~(#)~~<br/>I",["tsn", "ffssh", "zzz"]);
 const LanciaMiniRazzi = new Arma("Lanciaminirazzi",384,92,16,120,true,"‼<br/>≡≡Ø≡≡<br/>‼",["vsh!", "c-clck", "c-clck"]);
